8bd132d6a78479494b2db799a6585798d104a234
Ainur <konogalievaa@gmail.com>
2026-07-20 02:39:58 +0600

    StringX

---
+++ src/main/java/org/example/warmup2/StringX.java
@@ -0,0 +1,25 @@
+package org.example.warmup2;
+
+public class StringX {
+    public static void main(String[] args) {
+        StringX obj = new StringX();
+        System.out.println(obj.stringX("xxHxix"));
+    }
+    public String stringX(String str) {
+        if (str.length() <= 2) {
+            return str;
+        }
+
+        StringBuilder result = new StringBuilder();
+        result.append(str.charAt(0));
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) != 'x') {
+                result.append(str.charAt(i));
+            }
+        }
+        result.append(str.charAt(str.length() - 1));
+
+        return result.toString();
+    }
+
+}
