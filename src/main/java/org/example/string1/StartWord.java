f8402762b54334ef42990ebd7fa14535655bf3a2
Ainur <konogalievaa@gmail.com>
2026-07-20 03:26:36 +0600

    StartWord

---
+++ src/main/java/org/example/string1/StartWord.java
@@ -0,0 +1,16 @@
+package org.example.string1;
+
+public class StartWord {
+    public static void main(String[] args) {
+        StartWord obj = new StartWord();
+        System.out.println(obj.startWord("hippo", "hi"));
+    }
+    public String startWord(String str, String word) {
+        if (str.length()<word.length()){
+            return "";
+        }
+        if (str.substring(1,word.length()).equals(word.substring(1))){
+            return str.substring(0,word.length());
+        } return "";
+    }
+}
