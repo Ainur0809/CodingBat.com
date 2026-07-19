24b55e0b12fc80850dda0ac1b2652fd7129a658f
Ainur <konogalievaa@gmail.com>
2026-07-20 03:57:06 +0600

    FrontPiece

---
+++ src/main/java/org/example/array1/FrontPiece.java
@@ -0,0 +1,12 @@
+package org.example.array1;
+
+public class FrontPiece {
+    public int[] frontPiece(int[] nums) {
+        if (nums.length <= 2) {
+            return nums;
+        } else {
+            return new int[] {nums[0], nums[1]};
+        }
+    }
+
+}
