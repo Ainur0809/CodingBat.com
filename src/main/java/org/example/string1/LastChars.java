d385e9c94abfa29a330122936b0dab359bbc89c9
Ainur <konogalievaa@gmail.com>
2026-07-20 03:17:44 +0600

    LastChars

---
+++ src/main/java/org/example/string1/LastChars.java
@@ -0,0 +1,18 @@
+package org.example.string1;
+
+public class LastChars {
+    public static void main(String[] args) {
+        LastChars obj = new LastChars();
+        System.out.println(obj.lastChars("last", "chars"));
+    }
+    public String lastChars(String a, String b) {
+        if (a.length()==0 && b.length()==0){
+            return "@@";
+        } else if (b.length()==0) {
+            return a.substring(0,1)+"@";
+        } else if (a.length()==0) {
+            return "@"+b.substring(b.length()-1);
+        } return a.substring(0,1)+b.substring(b.length()-1);
+    }
+
+}
