cbe3ac1821bd67ecda11aeb2bc8c6b4e4fad6c75
Ainur <konogalievaa@gmail.com>
2026-07-20 01:03:24 +0600

    In3050

---
+++ src/main/java/org/example/warmup1/In3050.java
@@ -0,0 +1,13 @@
+package org.example.warmup1;
+
+public class In3050 {
+    public static void main(String[] args) {
+        In3050 obj = new In3050();
+        System.out.println(obj.in3050(30,31));
+    }
+    public boolean in3050(int a, int b) {
+        if (a>=30 && a<=40 && b>=30 && b<=40 || a>=40 && a<=50 && b>=40 && b<=50){
+            return true;
+        } return false;
+    }
+}
