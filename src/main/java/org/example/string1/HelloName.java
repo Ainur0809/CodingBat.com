153909ac9f98c82dded2b37783df7c95917d66e5
Ainur <konogalievaa@gmail.com>
2026-07-20 02:53:20 +0600

    HelloName

---
+++ src/main/java/org/example/string1/HelloName.java
@@ -0,0 +1,12 @@
+package org.example.string1;
+
+public class HelloName {
+    public static void main(String[] args) {
+        HelloName obj = new HelloName();
+        System.out.println(obj.helloName("Bob"));
+    }
+    public String helloName(String name) {
+        return "Hello "+ name+"!";
+    }
+
+}
