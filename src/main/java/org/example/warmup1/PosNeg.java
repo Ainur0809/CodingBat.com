bbee4dbc053bdbd1b0aeebeed1f4def547438a8d
Ainur <konogalievaa@gmail.com>
2026-07-20 00:30:03 +0600

    PosNeg

---
+++ src/main/java/org/example/warmup1/PosNeg.java
@@ -0,0 +1,14 @@
+package org.example.warmup1;
+
+public class PosNeg {
+    public static void main(String[] args) {
+        PosNeg obj = new PosNeg();
+        System.out.println(obj.posNeg(1,-1, false));
+    }
+    public boolean posNeg(int a, int b, boolean negative) {
+        if (!negative && (a>0 && b<0 || a<0 && b>0) || (negative && (a<0 && b<0))) {
+            return true;
+        }
+        return false;
+    }
+}
