32d317e72c8e8414937fd957a2db06a8bfb130d4
Ainur <konogalievaa@gmail.com>
2026-07-20 00:42:15 +0600

    BackAround

---
+++ src/main/java/org/example/warmup1/BackAround.java
@@ -0,0 +1,11 @@
+package org.example.warmup1;
+
+public class BackAround {
+    public static void main(String[] args) {
+        BackAround obj = new BackAround();
+        System.out.println(obj.backAround("cat"));
+    }
+    public String backAround(String str) {
+        return str.substring(str.length()-1) + str + str.substring(str.length()-1);
+    }
+}
